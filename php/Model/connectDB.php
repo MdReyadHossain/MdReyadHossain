90edf472c1a006b96072730562cff24ab23be420
Reyad <69958555+MdReyadHossain@users.noreply.github.com>
2023-08-18 14:55:26 +0600

    skills added

--- php/Model/connectDB.php
+++ php/Model/connectDB.php
@@ -1,19 +1,22 @@
 <?php
-    function connect() {
-        // $server = "sql203.ezyro.com";
-        // $db_user = "ezyro_32492792";
-        // $db_pass = "reyadserver";
-        // $dbname = "ezyro_32492792_reyad";
-        $server = "localhost";
-        $db_user = "root";
-        $db_pass = "";
-        $dbname = "reyad";
-        $reyad = new mysqli($server, $db_user, $db_pass, $dbname);
+function connect()
+{
+    // $server = "sql203.ezyro.com";
+    // $db_user = "ezyro_32492792";
+    // $db_pass = "reyadserver";
+    // $dbname = "ezyro_32492792_reyad";
 
-        if ($reyad->connect_error) {
-            die("Data base Connection failed: " . $reyad->connect_error);
-        }
+    /* -- x -- */
 
-        return $reyad;
+    $server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $dbname = "reyad";
+    $reyad = new mysqli($server, $db_user, $db_pass, $dbname);
+
+    if ($reyad->connect_error) {
+        die("Data base Connection failed: " . $reyad->connect_error);
     }
-?>
+
+    return $reyad;
+}
